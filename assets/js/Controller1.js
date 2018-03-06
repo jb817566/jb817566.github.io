1b1600b5e59585c39b2b42ea575cfe4a77e4c6db
Jay Bhagat <hhbhagat1417@gmail.com>
2018-03-06 00:51:26 -0500

    site

---
+++ assets/js/Controller1.js
@@ -0,0 +1,9 @@
+(function () {
+    angular.module('ExampleController', ['$scope']).controller('ExampleController', function ($scope) {
+
+        var vm = $scope;
+
+        vm.Hello = "Hellasdfdsadf";
+
+    })
+}())
